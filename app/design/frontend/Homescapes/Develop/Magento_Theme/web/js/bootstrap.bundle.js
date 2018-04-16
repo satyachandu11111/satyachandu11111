6824e2ce2b6660aa826781b85092c275c8dbadfe
BhsDesigner <manoj@homescapesonline.com>
2018-04-16 11:50:20 +0530

    footer css updated

--- app/design/frontend/Homescapes/Develop/Magento_Theme/web/js/bootstrap.bundle.js
+++ app/design/frontend/Homescapes/Develop/Magento_Theme/web/js/bootstrap.bundle.js
@@ -6343,7 +6343,27 @@ var Tab = function ($$$1) {
             return false
         })
     })
-}())							   
+}())
+							   
+// mega-menu js ends here
+							   
+// back to top js 
+							   
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+							   
+// back to tops ends here
 
 exports.Util = Util;
 exports.Alert = Alert;
