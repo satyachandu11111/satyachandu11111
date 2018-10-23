f2a145f125b555012a2625e04c65e1f9c7ee29e7
Ketan Patel <ketan@krishtechnolabs.com>
2018-10-23 10:43:46 +0530

    remove welpixel extensions

--- app/code/WeltPixel/OwlCarouselSlider/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'WeltPixel_OwlCarouselSlider',
-    __DIR__
-);
